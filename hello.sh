4e2d8c9ac13cdf4e235a1bf2cc63a3a82a2981bf
Adam Maryniuk <adamaze@gmail.com>
2022-09-04 16:09:30 -0400

    test for jess

--- hello.sh
+++ hello.sh
@@ -1,6 +1,6 @@
 function handler () {
     EVENT_DATA=$1
 
-    RESPONSE="{\"statusCode\": 200, \"body\": \"Hello from GitHub\"}"
+    RESPONSE="{\"statusCode\": 200, \"body\": \"Hello Jess, from GitHub\"}"
     echo $RESPONSE
 }
