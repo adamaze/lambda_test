ab32c9368551e54037d2c87aed9a8d6c82784254
Adam Maryniuk <adamaze@gmail.com>
2022-09-05 23:31:29 -0400

    make function actually do something...

--- functions/hello.sh
+++ functions/hello.sh
@@ -1,6 +1,7 @@
 function handler () {
     EVENT_DATA=$1
-
-    RESPONSE="{\"statusCode\": 200, \"body\": \"this is from a full-auto deployment\"}"
+    
+    body=$(curl -s https://lmguide.grenergy.com/ | grep Residential | head -1 | awk -F'<' '{print $(NF-1)}' | awk -F'>' '{print $NF}')
+    RESPONSE="{\"statusCode\": 200, \"body\": \"$body\"}"
     echo $RESPONSE
 }
