1e9422863d70e2ac6c1b17fbb2434ef4855e94e4
Adam Maryniuk <adamaze@gmail.com>
2022-09-07 00:18:08 -0400

    add env vars to func

--- functions/hello.sh
+++ functions/hello.sh
@@ -1,7 +1,9 @@
 function handler () {
     EVENT_DATA=$1
-    
-    body=$(curl -s https://lmguide.grenergy.com/ | grep Residential | head -1 | awk -F'<' '{print $(NF-1)}' | awk -F'>' '{print $NF}')
-    RESPONSE="{\"statusCode\": 200, \"body\": \"$body\"}"
+    site_data=$(curl -s https://lmguide.grenergy.com/)
+    results=$(echo "$site_data" | grep Residential | head -1 | awk -F'<' '{print $(NF-1)}' | awk -F'>' '{print $NF}')
+    echo $results > results
+    aws s3 cp results s3:/${bucket}/
+    RESPONSE="{\"statusCode\": 200, \"body\": \"$env\"}"
     echo $RESPONSE
 }
