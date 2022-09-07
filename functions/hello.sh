4491e63270d7eeb0f908ced9ec2e72167f0e2491
Adam Maryniuk <adamaze@gmail.com>
2022-09-07 00:27:36 -0400

    use /tmp

--- functions/hello.sh
+++ functions/hello.sh
@@ -2,8 +2,8 @@ function handler () {
     EVENT_DATA=$1
     site_data=$(curl -s https://lmguide.grenergy.com/)
     results=$(echo "$site_data" | grep Residential | head -1 | awk -F'<' '{print $(NF-1)}' | awk -F'>' '{print $NF}')
-    echo $results > results
-    aws s3 cp results s3:/${bucket}/
+    echo $results > /tmp/results
+    aws s3 cp /tmp/results s3:/${bucket}/
     RESPONSE="{\"statusCode\": 200, \"body\": \"$env\"}"
     echo $RESPONSE
 }
